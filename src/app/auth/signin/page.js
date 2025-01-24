e6d192567568788ff46461e6870785df0f196a78
Dawid Glapiak <dg18@live.it>
2025-01-25 00:31:53 +0100

    updated redirect to signin

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -29,7 +29,9 @@ const SignIn = () => {
     <div className="min-h-screen bg-gradient-to-br from-pink-500 via-purple-500 to-blue-500 flex flex-col items-center justify-center">
       {/* Logo */}
       <div className="absolute top-6 left-8">
-        <h1 className="text-3xl font-bold text-white">Webomo</h1>
+        <h1 className="text-3xl font-bold text-white">
+          Webomo <br /> Business{" "}
+        </h1>
       </div>
 
       {/* Login Form */}
